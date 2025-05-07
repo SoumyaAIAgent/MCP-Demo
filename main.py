44a22c2249d9aae73a12ab40101a53a502b7bbb0
Soumya Shrivastav <Soumya.sgsits@gmail.com>
2025-05-07 15:03:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,7 @@ def main():
             connection.handle_packets()
             
     except Exception as e:
+         print(f"Error starting server: {e}")
+
+if __name__ == "__main__":
+    main()
